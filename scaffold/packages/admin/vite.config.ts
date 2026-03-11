c4b26d9336a67e3c4398abd8612fa37fe9c0f1fb
amo <amo@fish.com>
2026-03-11 15:07:47 +0800

    chore(admin): add api

--- scaffold/packages/admin/vite.config.ts
+++ scaffold/packages/admin/vite.config.ts
@@ -1,7 +1,7 @@
-import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
+import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +9,18 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@scaffold/api/client": path.resolve(__dirname, "../api/dist/client/index.ts"),
       "@scaffold/core": path.resolve(__dirname, "../core/src/index.ts"),
     },
   },
-});
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        // rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+})
